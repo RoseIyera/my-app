413c9c3a61b92775bbf4b3d27e89a8a1c9aad584
Rose Iyera <roseiyera@gmail.com>
2024-01-25 20:35:08 +0200

    Trying to fix background

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -7,11 +7,13 @@ import Cover from '../../assets/Cover.png'
 
 const Home = () => {
     const redirect=useNavigate();
+
+
   return (
     <div className='home'>
         <div className="landing-page">
         <div><Button className="explore-button" variant='outlined' color='primary' onClick={()=>redirect("/products")}>
-            Cake List
+            View Cake List
         </Button></div>
         <img src={Cover} alt="Cake Background" />
         </div>

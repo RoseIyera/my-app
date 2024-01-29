4a5baf89a76f5e6ee79edd075af69ce629d4fdff
Rose Iyera <roseiyera@gmail.com>
2024-01-29 18:16:27 +0200

    Adding Auth0 functionality

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,9 +3,15 @@ import { useState } from 'react';
 import {Menu, Close} from "@mui/icons-material";
 import {Link} from "react-router-dom";
 import Logo from '../../assets/Logo.png'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Navbar: React.FC = () => {
     const [open, setOpen]=  useState<boolean>(false);
+    const { loginWithRedirect, isAuthenticated, user } = useAuth0();
+    const { logout } = useAuth0();
+
+    //Vendor-Admin Check if the user is authenticated and the email is the specific one
+    const isVendorUser = isAuthenticated && user?.email === 'roseiyera@gmail.com';
 
     const toggleNavBar = () => {
         if(window.innerWidth < 500){
@@ -30,34 +36,56 @@ const Navbar: React.FC = () => {
                 <li onClick={toggleNavBar}>
                     <Link to="">Home</Link>
                 </li>
+
+                <li onClick={toggleNavBar}>
+                    <Link to="">View Products</Link>
+                </li>
                 
                 {/* Admin section */}
-
+                {isVendorUser && (
                 <li onClick={toggleNavBar}>
                     <Link to="/products">Products</Link>
                 </li>
+                )}
+
+                {isVendorUser && (
                 <li onClick={toggleNavBar}>
                     <Link to="/products/add">Add Product</Link>
                 </li>
+                )}
+
+                {isVendorUser && (
                 <li>
                     <Link to="">View Customer Orders</Link>
                 </li>
+                )}
 
                 {/* Customer Section */}
+                {!isAuthenticated && (
                 <li>
                     <Link to="/order">Order</Link>
                 </li>
+                )}
+
+                {!isAuthenticated && (
                 <li>
                     <Link to="/myorders">View my Orders</Link>
                 </li>
+                )}
 
                 {/* Authentication */}
-                <li>
+                {!isAuthenticated && (
+                <li onClick={() => loginWithRedirect()}>
                     <Link to="/login" className='outerBtn'>Login</Link>
                 </li>
-                <li>
+                )}
+
+                {isAuthenticated && (
+                <li onClick={() => logout()}>
                     <Link to="" className='outerBtn'>Logout</Link>
                 </li>
+                 )}
+
             </ul>
         </div>
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {Routes,Route} from 'react-router-dom';
+import { createRoot} from 'react-dom/client'
 import './global.scss';
 import App from './App';
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Router} from "react-router-dom"
+import reportWebVitals from './reportWebVitals';
+import { Auth0Provider, Auth0ProviderOptions } from '@auth0/auth0-react';
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const auth0ProviderOptions: Auth0ProviderOptions = {
+  domain: domain!,
+  clientId: clientId!,
+  //redirectUri: window.location.origin,
+};
+
+const root = createRoot(document.getElementById('root')!);
+
 root.render(
-  <BrowserRouter>
-    <App />
+  <BrowserRouter >
+     <Auth0Provider {...auth0ProviderOptions}>
+      <App />
+      </Auth0Provider>
   </BrowserRouter>
-    
-
+ 
 );
